24edfe7bc6b6404fa7b24004d6fcf4f675b909ba
shane <fanxin_1973@yahoo.com>
2018-01-31 06:38:40 +1100

    add input in login.component

--- trunk/plaque_node/angular/src/app/app.module.ts
+++ trunk/plaque_node/angular/src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from "./components/login.component";
 import { PanelComponent } from "./components/panel.component";
 import { MatButtonModule } from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent, LoginComponent, PanelComponent],
-    imports: [BrowserModule, routing, MatButtonModule],
+    imports: [BrowserModule, routing, BrowserAnimationsModule,
+        MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule],
     bootstrap: [AppComponent]
 })
 export class AppModule { };
--- trunk/plaque_node/angular/src/app/components/login.component.ts
+++ trunk/plaque_node/angular/src/app/components/login.component.ts
@@ -2,9 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import {MatButtonModule} from '@angular/material';
 
+
 @Component({
     selector: 'login',
-    template: '<h1>{{text}}</h1><button mat-raised-button (click)="login()" value="login">Login</button>'
+    template: `
+     <div>
+        <form class="example-container">
+            <mat-form-field>
+                    <input matInput floatPlaceholder="never"
+                        placeholder="User Email" required>
+            </mat-form-field>
+            <mat-form-field>
+                    <input matInput floatPlaceholder="never"
+                        placeholder="Password" required>
+            </mat-form-field>
+        </form>
+    </div>
+  `
 })
 export class LoginComponent implements OnInit {
 
